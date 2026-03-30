ae10f84dd5e386b61ad3dc5b2416abbdb349a36e
belalkhatab <belalkhtab78@gmail.com>
2026-03-30 22:57:48 +0200

    n

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let a=10
-let b=20
-let c=a+b
-console.log(c)
+const person = {
+    name: {
+        name1: "John",
+        name2: "Doe"
+    },
+    age: 30,
+    city: "New York"
+};
+console.log(person.name.name1); // Output: John
+console.log(person.name.name2); // Output: Doe
+console.log(person.age);  // Output: 30
+console.log(person.city); // Output: New York
+person.name.name1 = "belal";
+console.log(person.name.name1); // Output: belal
